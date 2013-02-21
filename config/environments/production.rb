f78c6b481e8df524fac7fabd63fd2708d6983d01
Thiago von Sydow <thiagovs05@gmail.com>
2013-02-21 19:22:56 -0300

    ajuste emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ ControlePontoEletronico::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
+  config.action_mailer.default_url_options = { :host => 'controle-de-ponto.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :user_name => "paguearep",
