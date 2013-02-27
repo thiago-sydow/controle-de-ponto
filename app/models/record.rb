3e6001fac21aaf8fbc3596d116211bbfe68f3473
Thiago von Sydow <thiagovs05@gmail.com>
2013-02-27 18:23:31 -0300

    Considerado hora atual. Closes #6

--- app/models/record.rb
+++ app/models/record.rb
@@ -7,21 +7,28 @@ class Record
 
   def self.total_worked_hours(records)
   	@firstRecord = records.first
-    @totalHours = 0
-    @totalMinutes = 0
+    @totalHours = Time.new(DateTime.now.year, DateTime.now.month, DateTime.now.day, 0, 0)    
+    @lastRecord = records.last
 
     records.each_with_index do |record, index|
       @time_diff_components = Time.diff(Time.parse(@firstRecord.time.to_s), Time.parse(record.time.to_s))
 
       if index.odd?
-        @totalHours += @time_diff_components[:hour]
-        @totalMinutes += @time_diff_components[:minute]
+        @totalHours += @time_diff_components[:hour].hours
+        @totalHours += @time_diff_components[:minute].minutes
       end
 
       @firstRecord = record
     end
+   
 
-    @total_hours = DateTime.new(DateTime.now.year, DateTime.now.month, DateTime.now.day, @totalHours, @totalMinutes)
+    if records.size.odd?
+      @diff = Time.diff(Time.parse(@lastRecord.time.to_s), Time.now)
+      @totalHours += @diff[:hour].hours
+      @totalHours += @diff[:minute].minutes
+    end
+
+    @totalHours
   end
 
 end
