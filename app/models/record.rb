fa8435b62093faa2f170fd84c6bdd0be2942258d
Thiago von Sydow <thiagovs05@gmail.com>
2015-02-04 16:46:32 -0200

    added default scope to record

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,9 +2,9 @@ class Record
   include Mongoid::Document
 
   field :time, type: DateTime
-
   belongs_to :user
-
   validates_presence_of :time, :user_id
+
+  default_scope -> { asc(:time) }
 
 end
