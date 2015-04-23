036ea16243bfb81479e509a61d89cbc9196b411c
Thiago von Sydow <thiagovs05@gmail.com>
2015-04-22 23:33:27 -0300

    changed presenter to use closure

--- app/presenters/dashboard_presenter.rb
+++ app/presenters/dashboard_presenter.rb
@@ -1,21 +1,21 @@
 class DashboardPresenter < Burgundy::Item
 
   def total_balance
-    @total_balance ||= day_records.inject(TimeBalance.new) { |sum_balance, day| sum_balance.sum(day.balance) }
+    @total_balance ||= days_since_last_closure.inject(TimeBalance.new) { |sum_balance, day| sum_balance.sum(day.balance) }
   end
 
   def total_worked
-    @total_worked ||= current_day_record.try(:total_worked) || Time.current.midnight
+    @total_worked ||= current_day_record.try(:total_worked) || DayRecord::ZERO_HOUR
   end
 
   def departure_time
-    @departure_time = current_day_record.try(:forecast_departure_time) || Time.current.midnight
+    @departure_time = current_day_record.try(:forecast_departure_time) || DayRecord::ZERO_HOUR
   end
 
   def percentage_worked
-    base_time = Time.zone.local(1999, 8, 1).midnight
-    time_1 = Time.zone.local(1999, 8, 1).change(hour: workload.hour, min: workload.min)
-    time_2 = Time.zone.local(1999, 8, 1).change(hour: total_worked.hour, min: total_worked.min)
+    base_time = DayRecord::ZERO_HOUR
+    time_1 = base_time.change(hour: workload.hour, min: workload.min)
+    time_2 = base_time.change(hour: total_worked.hour, min: total_worked.min)
 
     (( (time_2 - base_time) / (time_1 - base_time) ) * 100).round(1)
   end
@@ -24,6 +24,10 @@ class DashboardPresenter < Burgundy::Item
 
   def current_day_record
     @current_day_record ||= day_records.where(reference_date: Date.current).first
+  end
+
+  def days_since_last_closure
+    day_records.where(:reference_date.gte => closures.last.end_date)
   end
 
 end
