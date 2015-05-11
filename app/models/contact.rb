669a09ff3e86afb67ff281842cefca845755304a
Thiago von Sydow <thiagovs05@gmail.com>
2015-05-11 01:12:54 -0300

    added contact form and fixed style issues

---
+++ app/models/contact.rb
@@ -0,0 +1,17 @@
+class Contact < MailForm::Base
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+
+  attribute :message
+  attribute :nickname,  :captcha  => true
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => "Contato do Site",
+      :to => "contato@meucontroledeponto.com.br",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
