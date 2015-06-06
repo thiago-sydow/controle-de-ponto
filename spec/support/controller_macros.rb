120b5029c5146fc47bfe1891ff89e2a5a8394946
Thiago von Sydow <thiagovs05@gmail.com>
2015-06-06 19:10:35 -0300

    fixed deprecation warning

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,7 +2,7 @@ module ControllerMacros
   def login_user
     before(:each) do
       @request.env['devise.mapping'] = Devise.mappings[:user]
-      user.confirm!
+      user.confirm
       sign_in user
     end
   end
