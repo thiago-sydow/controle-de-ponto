0835a60f8f2bda94aa35cc4e8caa5c26e1a4b88c
Thiago von Sydow <thiagovs05@gmail.com>
2015-12-29 15:58:39 -0200

    shchema

---
+++ db/schema.rb
@@ -0,0 +1,86 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20151227205759) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "type"
+    t.string   "name",                        null: false
+    t.integer  "workload",    default: 28800, null: false
+    t.integer  "user_id"
+    t.jsonb    "preferences", default: {},    null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  add_index "accounts", ["preferences"], name: "index_accounts_on_preferences", using: :gin
+
+  create_table "closures", force: :cascade do |t|
+    t.date     "start_date", null: false
+    t.date     "end_date",   null: false
+    t.integer  "account_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "day_records", force: :cascade do |t|
+    t.date     "reference_date",      null: false
+    t.text     "observations"
+    t.string   "work_day"
+    t.string   "missed_day"
+    t.string   "medical_certificate"
+    t.integer  "account_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
+
+  create_table "time_records", force: :cascade do |t|
+    t.datetime "time",          null: false
+    t.integer  "day_record_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "gender"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "birthday"
+    t.integer  "current_account_id"
+  end
+
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["current_account_id"], name: "index_users_on_current_account_id", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+end
