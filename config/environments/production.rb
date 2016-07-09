2c54e0cb17025087aab39f746f8b62cd58f4fee3
Thiago von Sydow <thiagovs05@gmail.com>
2016-07-09 13:12:00 -0300

    fix env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :redis_store, "ENV['REDIS_URL']/meucontroledeponto/cache"
+  config.cache_store = :redis_store, "#{ENV['REDIS_URL']}/meucontroledeponto/cache"
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
