12df4e9bf187fb3efa2ff143e3f385b043dfd916
Thiago von Sydow <thiagovs05@gmail.com>
2017-06-26 22:24:45 +0200

    call ajax function in random time

--- app/assets/javascripts/ajax_update.js
+++ app/assets/javascripts/ajax_update.js
@@ -5,7 +5,10 @@ var updateTime = function() {
     var chart = $('.easy-pie-chart.percentage');
     chart.data('easyPieChart').update(response.percentage);
     chart.find('.percent').text(response.percentage);
-    setTimeout(updateTime, 60000);
+    
+    // Random between 1 and 3 minutes
+    var milliseconds = Math.floor(Math.random() * (180 - 60)) + 60;
+    setTimeout(updateTime, milliseconds * 1000);
   });
 };
 
