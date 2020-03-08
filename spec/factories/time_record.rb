64c822f3648b4b8a2011aca8f72ec374d3a25d9c
Thiago von Sydow <thiagovs05@gmail.com>
2020-03-08 12:41:54 +0100

    Atulizar gems (#261)
    * bum gem versions
    
    * update faker method call
    
    Co-authored-by: Thiago von Sydow <thiago.vonsydow@tellcharlie.com>

--- spec/factories/time_record.rb
+++ spec/factories/time_record.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :time_record do
     day_record
-    time { Faker::Time.between(Time.current.midnight, Time.current.change(hour: 23, minute: 59)) }
+    time { Faker::Time.between(from: Time.current.midnight, to: Time.current.change(hour: 23, minute: 59)) }
   end
 
 end
